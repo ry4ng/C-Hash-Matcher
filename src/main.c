bef676da225fe3fe1fbecd11419a6ea94ebeae6a
Ryan <ryan.smith4@sky.uk>
2017-04-29 19:55:21 +0100

    v1.1
    User can provide arguments to output different information.
    Improved ‘hashes checked’ functionality.
    Cleaned up code.

--- src/main.c
+++ src/main.c
@@ -7,15 +7,7 @@
 
 #include "sha256.h"
 
-// void print_hash(unsigned char hash[])
-// {
-//     int i;
-//     for (i=0; i < 32; i++)
-//         printf("%02x",hash[i]);
-//     printf("\n");
-// }
-
-int main()
+int main(int argc, char *argv[])
 {
     clock_t start = clock();
     setlocale(LC_NUMERIC, "");
@@ -24,28 +16,25 @@ int main()
     // char targetHash[65] = "8990d5f8063b0f92bf863fa819cee92e93dd1630cb368e4f6959536a7165f2a9"; //94,010,302
     char targetHash[65] = "8cd178e5738e3042f4ff9cf1f9c86d38980b50074066b4873ef126c6a2c6815a"; //REAL TARGET
 
-    unsigned long long startingInt = 1240798589; // 1,240,798,589
-    unsigned long long tartgetInt = 10000000000; // 10,000,000,000
+    unsigned long long startingInt = 0; // 1,240,798,589
+    unsigned long long tartgetInt = 100000; // 10,000,000,000
 
+    unsigned long long counterInt;
     unsigned char hexOfInt[32];
     unsigned char hash[32];
 
-    unsigned int counter;
-
     FILE *f = fopen("../hashes.txt", "w");
 
-    printf("\nStarting at: %'llu\n", startingInt);
+    printf("\nStarting at:  %'llu\n", startingInt);
     printf("Finishing at: %'llu\n", tartgetInt);
     printf("------------------------------------------------------------------------------\n");
     printf("Target hash:  %s\n\n", targetHash);
+    printf("Hashing...\n");
 
-    while (startingInt < tartgetInt) {
+    while (counterInt < tartgetInt) {
 
-        sprintf(hexOfInt, "%x", startingInt);
-
-        // Prints Raw Value
-        // printf("%s \n", hexOfInt);
-
+        // Converts counterInt to hex & hashes
+        sprintf(hexOfInt, "%x", counterInt);
         SHA256_CTX ctx;
         sha256_init(&ctx);
         sha256_update(&ctx,hexOfInt,strlen(hexOfInt));
@@ -59,40 +48,44 @@ int main()
             strcat(hashValue, string);
         }
 
+        // Writes hashing results to txt file
+        fprintf(f, "\n%s\n", hexOfInt);
+        fprintf(f, "%'llu\n", counterInt);
+        fprintf(f, "%s\n", hashValue);
+
+        // Checks if the resulting hash is equal to the target hash
         if (strcmp(hashValue, targetHash) == 0) {
             system("clear");
             printf("----------------------------------------------------------------------------\n");
             printf("A match for your target hash hash been found!\n\n");
             printf("Raw value:  %s\n", hexOfInt);
-            printf("Int value:  %'llu\n", startingInt);
+            printf("Int value:  %'llu\n", counterInt);
             printf("Hash value: %s\n", hashValue);
             printf("----------------------------------------------------------------------------\n");
             break;
         } else {
-            // Prints hashed value
-            // printf("%s \n", hashValue);
-        }
-
-        // Writes hashing results to file
-        fprintf(f, "\n%s\n", hexOfInt);
-        fprintf(f, "%'llu\n", startingInt);
-        fprintf(f, "%s\n", hashValue);
-
-        counter++;
-        if (counter >= 1000) {
-            system("clear");
-            printf("Currently on: ");
-            printf("%'llu\n", startingInt);
-            counter = 0;
+            if (argc > 1) {
+                if (strcmp(argv[1], "log-all") == 0){
+                    printf("\n%'llu\n", counterInt);
+                    printf("%s\n", hexOfInt);
+                    printf("%s\n", hashValue);
+                } else if (strcmp(argv[1], "log-count") == 0) {
+                    printf("%'llu\n", counterInt);
+                } else if (strcmp(argv[1], "log-hex") == 0) {
+                    printf("%s\n", hexOfInt);
+                } else if (strcmp(argv[1], "log-hash") == 0) {
+                    printf("%s\n", hashValue);
+                }
+            }
         }
 
         // Clear hashValue
         strncpy(hashValue, "", sizeof(hashValue));
-        startingInt++;
+        counterInt++;
     }
 
     clock_t finish = clock();
-    printf("\n%'llu hashes checked in %'f seconds.\n\n", tartgetInt - startingInt, (double)(finish - start) / CLOCKS_PER_SEC);
+    printf("\n%'llu hashes checked in %'f seconds.\n\n", (tartgetInt - startingInt), (double)(finish - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
